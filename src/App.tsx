cf4112cef847d03836e33749e9bfeff1c5bedbe2
Ananta <codeantu@gmail.com>
2024-07-18 21:06:36 +0530

    chore: Update header styling and remove unused CSS import

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export function App() {
     const end = performance.now();
     setStart(start);
     setEnd(end);
-    // if (temp == undefined) return setComponent([] as Token[]); 
+    // if (temp == undefined) return setComponent([] as Token[]);
     setComponent(temp);
   }
 
@@ -121,7 +121,7 @@ export function App() {
 
   function Header() {
     return (
-      <header className='sticky top-0 flex items-center justify-center border-b border-gray-800 bg-[#0b112175] p-3 backdrop-blur-xl'>
+      <header className='sticky top-0 z-50 flex items-center justify-center border-b border-gray-800 bg-[#0b112175] p-3 backdrop-blur-xl'>
         <div className='flex max-w-7xl flex-grow items-center justify-between'>
           <div className='text-xl font-semibold text-white'>Markdown compiler</div>
           <div className='flex flex-grow items-center justify-end'>
@@ -222,7 +222,6 @@ export function App() {
                     clipRule='evenodd'
                   />
                 </svg>
-                <span className='sr-only'>Facebook page</span>
               </a>
               <a
                 target='_blank'
@@ -236,7 +235,6 @@ export function App() {
                     clipRule='evenodd'
                   />
                 </svg>
-                <span className='sr-only'>Instagram page</span>
               </a>
               <a
                 target='_blank'
@@ -246,7 +244,6 @@ export function App() {
                 <svg className='h-5 w-5' fill='currentColor' viewBox='0 0 24 24' aria-hidden='true'>
                   <path d='M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84' />
                 </svg>
-                <span className='sr-only'>Twitter page</span>
               </a>
               <a
                 target='_blank'
@@ -260,7 +257,6 @@ export function App() {
                     clipRule='evenodd'
                   />
                 </svg>
-                <span className='sr-only'>GitHub account</span>
               </a>
             </div>
           </div>
